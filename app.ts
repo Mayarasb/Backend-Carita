ebf9700fdd5d9935e1dd535153dd71c221ed62c8
arieleperes <ariele.peres@fatec.sp.gov.br>
2025-05-28 22:07:58 -0300

    add autenticação

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import organizacaoRoutes from "./src/routes/organizacao.routes";
 import parceiroRoutes from "./src/routes/parceiro.routes"
 import  pontoArrecadacaoRoutes from "./src/routes/pontoArrecadacao.routes"
 import usuarioRoutes from "./src/routes/usuario.routers"
+import { authRouter } from "./src/routes/auth.routes";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use("/organizacao", organizacaoRoutes);
 app.use("/parceiros",parceiroRoutes)
 app.use("/pontosArrecadacao",pontoArrecadacaoRoutes)
 app.use("/usuarios",usuarioRoutes)
+app.use("/autenticacao", authRouter)
 
 app.listen(3000, () => {
     console.log("Servidor executando na porta 3000");
