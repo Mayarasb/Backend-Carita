36100d114543032cca0d2c1a0a62c4e2b3775285
Vanessa-Nobrega <vanessacapuano1@gmail.com>
2025-06-24 15:29:12 -0300

    Atualizar caminho para banco de dados

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize ({
     dialect: "sqlite",
-    storage: "./database.sqlite",
+    // storage: "./database.sqlite", Atualizar o caminho do banco de dados
+    storage: "./src/config/database.sqlite",
     logging: true,
 });
 
